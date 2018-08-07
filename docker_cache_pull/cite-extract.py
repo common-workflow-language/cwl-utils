d15c01644c80308b2eef9045c8a7e2ec76ef8395
Miguel Boland <mdb@ebi.ac.uk>
2018-08-07 14:20:39 +0100

    Tool to read download docker images for a required workflow and generate commands to import these into udocker

---
+++ docker_cache_pull/cite-extract.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sys
+import CommonWorkflowLanguage as cwl
+
+def main():
+    top = cwl.load_document(sys.argv[1])
+    traverse(top)
+
+def extract_software_packages(process: cwl.Process):
+    for req in extract_software_reqs(process):
+        print(process.id)
+        process_software_requirement(req)
+
+def extract_software_reqs(process: cwl.Process):
+    if process.requirements:
+        for req in process.requirements:
+            if isinstance(req, cwl.SoftwareRequirement):
+                yield req
+    if process.hints:
+        for req in process.hints:
+            if req['class'] == "SoftwareRequirement":
+                yield cwl.load_field(req, cwl.SoftwareRequirementLoader,
+                                     process.id, process.loadingOptions)
+
+def process_software_requirement(req: cwl.SoftwarePackage):
+    for package in req.packages:
+        print("Package: {}, version: {}, specs: {}".format(
+              package.package, package.version, package.specs))
+
+
+def traverse(process: cwl.Process):
+    extract_software_packages(process)
+    if isinstance(process, cwl.Workflow):
+        traverse_workflow(process)
+
+def get_process_from_step(step: cwl.WorkflowStep):
+    if isinstance(step.run, str):
+        return cwl.load_document(step.run)
+    return step.run
+
+def traverse_workflow(workflow: cwl.Workflow):
+    for step in workflow.steps:
+        extract_software_packages(step)
+        traverse(get_process_from_step(step))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ docker_cache_pull/docker-extract.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+import sys
+import os
+import CommonWorkflowLanguage as cwl
+import subprocess
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tool to save docker images from a cwl workflow \n '
+                                                 'and generate udocker loading commands.')
+    parser.add_argument('dir', help='Directory in which to save images')
+    parser.add_argument('input', help='Input CWL workflow')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    os.makedirs(args.dir, exist_ok=True)
+
+    top = cwl.load_document(args.input)
+
+    for req in set(traverse(top)):
+        image_name = get_image_name(req)
+        save_docker_image(req, image_name, args.dir)
+
+        print(load_docker_image(image_name))
+
+
+def get_image_name(req):
+    return ''.join(req.split('/')) + '.tar'
+
+
+def load_docker_image(image_name):
+    return f'udocker load -i  {image_name}'
+
+
+def save_docker_image(req, image_name, image_dir):
+    cmd = ['docker', 'save', '-o', os.path.join(image_dir, image_name), req]
+    subprocess.run(" ".join(cmd), shell=True, check=True)
+
+
+def extract_docker_requirements(process: cwl.Process):
+    for req in extract_docker_reqs(process):
+        if ':' not in req.dockerPull:
+            req.dockerPull += ':latest'
+        yield req.dockerPull
+
+
+def extract_docker_reqs(process: cwl.Process):
+    if process.requirements:
+        for req in process.requirements:
+            if isinstance(req, cwl.DockerRequirement):
+                yield req
+    if process.hints:
+        for req in process.hints:
+            if req['class'] == "DockerRequirement":
+                yield cwl.load_field(req, cwl.DockerRequirementLoader,
+                                     process.id, process.loadingOptions)
+
+
+def process_docker_requirement(req: cwl.SoftwarePackage):
+    if ':' not in req.dockerPull:
+        req.dockerPull += ':latest'
+    return req.dockerPull
+
+
+def traverse(process: cwl.Process):
+    yield from extract_docker_requirements(process)
+    if isinstance(process, cwl.Workflow):
+        yield from traverse_workflow(process)
+
+
+def get_process_from_step(step: cwl.WorkflowStep):
+    if isinstance(step.run, str):
+        return cwl.load_document(step.run)
+    return step.run
+
+
+def traverse_workflow(workflow: cwl.Workflow):
+    for step in workflow.steps:
+        for req in extract_docker_reqs(step):
+            yield req
+        yield from traverse(get_process_from_step(step))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
