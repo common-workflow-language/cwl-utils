8ac8aa9688b0f82e3e9f3b64ac932e9fc67849b4
Gijs Molenaar <gijs@pythonic.nl>
2019-01-15 11:12:43 +0200

    remove requirements file

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+
+setup_requires = [
+    'ruamel.yaml',
+    'six',
+    'requests',
+    'cachecontrol',
+    'schema_salad',
+]
+
 setup(
     name='cwl-utils',
     version='0.1',
@@ -7,5 +16,5 @@ setup(
     author_email='common-workflow-language@googlegroups.com',
     packages=find_packages(),
     python_requires='>=3.6',
-    setup_requires=['ruamel.yaml', 'six'],
+    setup_requires=setup_requires,
 )
