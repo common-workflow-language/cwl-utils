b59078916b4cab661387e8fb10875aa16b148d73
Gijs Molenaar <gijs@pythonic.nl>
2019-01-23 14:35:22 +0100

    run tests, but skip docker test on travis

--- tests/test_docker_extract.py
+++ tests/test_docker_extract.py
@@ -1,14 +1,17 @@
-from unittest import TestCase
+from unittest import TestCase, skipIf
 from pathlib import Path
+from os import environ
 from tempfile import TemporaryDirectory
 from cwl_utils.docker_extract import traverse, get_image_name, save_docker_image, load_docker_image
 import cwl_utils.parser_v1_0 as parser
 
 HERE = Path(__file__).resolve().parent
 TEST_CWL = HERE / "../testdata/md5sum.cwl"
+TRAVIS = environ.get('TRAVIS', 'false') == 'true'
 
 
 class TestDockerExtract(TestCase):
+    @skipIf(TRAVIS, reason="travis doesn't running docker in docker")
     def test_traverse_workflow(self):
         loaded = parser.load_document(str(TEST_CWL.resolve()))
 
