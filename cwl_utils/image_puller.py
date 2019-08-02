47ba7b5bd0ed34ef47d710f3b84d3080311b5c50
Michael R. Crusoe <michael.crusoe@gmail.com>
2019-08-02 12:55:00 +0200

    Support both Singularity 2.6.1 and 3.x
    Fix the tests

--- cwl_utils/image_puller.py
+++ cwl_utils/image_puller.py
@@ -62,16 +62,35 @@ class SingularityImagePuller(ImagePuller):
     CHARS_TO_REPLACE = ['/', ':']
     NEW_CHAR = '-'
 
+    def __init__(self, req, save_directory):
+        super(SingularityImagePuller, self).__init__(req, save_directory)
+        version = subprocess.check_output(["singularity", "--version"], universal_newlines=True)
+        if version.startswith("singularity version "):
+            version = version[20:]
+        self.version = version
+
+    def _is_version_2_6(self):  # type: ()->bool
+        return self.version.startswith("2.6")
+
+    def _is_version_3_or_newer(self):  # type: ()->bool
+        return int(self.version[0]) >= 3
+
     def get_image_name(self):
         image_name = self.req
         for char in self.CHARS_TO_REPLACE:
             image_name = image_name.replace(char, self.NEW_CHAR)
-        return f'{image_name}.img'
+        if self._is_version_2_6():
+            suffix = ".img"
+        elif self._is_version_3_or_newer():
+            suffix = ".sif"
+        else:
+            raise Exception("Don't know how to handle this version of singularity: {}.".format(self.version))
+        return f'{image_name}.{suffix}'
 
     def save_docker_image(self):
         _LOGGER.info(f"Pulling {self.req} with Singularity...")
-        cmd_pull = ['singularity', 'pull', os.path.join(self.save_directory,
+        cmd_pull = ['singularity', 'pull', '--name', os.path.join(self.save_directory,
                                                         self.get_image_name()),
-                    f'docker:{self.req}']
+                    f'docker://{self.req}']
         ImagePuller._run_command_pull(cmd_pull)
         _LOGGER.info(f"Image successfully pulled: {self.save_directory}/{self.get_image_name()}")
