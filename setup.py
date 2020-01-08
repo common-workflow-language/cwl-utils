ded27c7c3bae3f6593c0aa8d36748a6e1b01baf1
Michael R. Crusoe <michael.crusoe@gmail.com>
2020-01-08 13:07:19 +0100

    0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cwl-utils',
-    version='0.1',
+    version='0.2',
     author='Common workflow language working group',
     author_email='common-workflow-language@googlegroups.com',
     packages=find_packages(),
