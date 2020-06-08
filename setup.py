3c5d409d5cd62ac4ba315b4d569e4e7c74841d57
Michael Franklin <michael.franklin@petermac.org>
2020-06-09 09:47:46 +1000

    Add extra metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
+exec(open("cwl_utils/__meta__.py").read())
 
 setup(
     name='cwl-utils',
-    version='0.3',
+    version=__version__,
     author='Common workflow language working group',
     author_email='common-workflow-language@googlegroups.com',
     packages=find_packages(),
@@ -20,4 +21,7 @@ setup(
     tests_require=['pytest'],
     test_suite='tests',
     scripts=['cwl_utils/docker_extract.py'],
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/common-workflow-language/cwl-utils',
 )
