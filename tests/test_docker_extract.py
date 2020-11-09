5306f6862fb629ac45fcbbc5d8f81821f45fc0d2
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2020-11-09 16:52:33 +0100

    expr refactor: better interface (#40)

--- tests/test_docker_extract.py
+++ tests/test_docker_extract.py
@@ -1,8 +1,9 @@
 from os import environ
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from unittest import TestCase, skipIf
+from unittest import skipIf
 
+import pytest
 import cwl_utils.parser_v1_0 as parser
 from cwl_utils.docker_extract import traverse
 from cwl_utils.image_puller import DockerImagePuller, SingularityImagePuller
@@ -12,22 +13,23 @@ TEST_CWL = HERE / "../testdata/md5sum.cwl"
 TRAVIS = environ.get("TRAVIS", "false") == "true"
 
 
-class TestDockerExtract(TestCase):
-    @skipIf(TRAVIS, reason="travis cannot run docker in docker")
-    def test_traverse_workflow(self):
-        loaded = parser.load_document(str(TEST_CWL.resolve()))
+@pytest.mark.skipif(TRAVIS, reason="travis cannot run docker in docker")
+def test_traverse_workflow() -> None:
+    """Test container extraction tool using Docker."""
+    loaded = parser.load_document(str(TEST_CWL.resolve()))
 
-        with TemporaryDirectory() as tmpdir:
-            for req in set(traverse(loaded)):
-                image_puller = DockerImagePuller(req.dockerPull, tmpdir)
-                image_puller.save_docker_image()
-                _ = image_puller.generate_udocker_loading_command()
+    with TemporaryDirectory() as tmpdir:
+        for req in set(traverse(loaded)):
+            image_puller = DockerImagePuller(req.dockerPull, tmpdir)
+            image_puller.save_docker_image()
+            _ = image_puller.generate_udocker_loading_command()
 
-    @skipIf(TRAVIS, reason="travis cannot run singularity in docker")
-    def test_traverse_workflow_singularity(self):
-        loaded = parser.load_document(str(TEST_CWL.resolve()))
+@pytest.mark.skipif(TRAVIS, reason="travis cannot run singularity in docker")
+def test_traverse_workflow_singularity() -> None:
+    """Test container extraction tool using Singularity."""
+    loaded = parser.load_document(str(TEST_CWL.resolve()))
 
-        with TemporaryDirectory() as tmpdir:
-            for req in set(traverse(loaded)):
-                image_puller = SingularityImagePuller(req.dockerPull, tmpdir)
-                image_puller.save_docker_image()
+    with TemporaryDirectory() as tmpdir:
+        for req in set(traverse(loaded)):
+            image_puller = SingularityImagePuller(req.dockerPull, tmpdir)
+            image_puller.save_docker_image()
