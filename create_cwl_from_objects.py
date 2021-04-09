6519118fe8b76e21e0d92c81bbda95cedc6b4fb1
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-04-09 17:00:30 +0200

    Example of programmatic CWL creation

---
+++ create_cwl_from_objects.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+import cwl_utils.parser_v1_2 as cwl
+
+from ruamel import yaml
+
+def main() -> None:
+    """Generate a CWL object to match "cat-tool.cwl"."""
+    inputs = [cwl.CommandInputParameter(id="file1", type="File")]
+    outputs = [
+        cwl.CommandOutputParameter(
+            id="output",
+            type="File",
+            outputBinding=cwl.CommandOutputBinding(glob="output"),
+        )
+    ]
+    cat_tool = cwl.CommandLineTool(
+        inputs=inputs,
+        outputs=outputs,
+        cwlVersion="v1.2",
+        baseCommand="cat",
+        stdin="$(inputs.file1.path)",
+        stdout="output",
+    )
+    print(yaml.main.round_trip_dump(cat_tool.save()))
+
+
+if __name__ == "__main__":
+    main()
