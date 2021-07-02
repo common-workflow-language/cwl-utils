f3fce3e8b087ce8291cf32bd5c24aff194289866
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-07-02 14:45:19 +0200

    bump cwl-upgrader version dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "typing_extensions",
         "cwltool >= 3.0.20201113183607",
         "cwlformat",
-        "cwl-upgrader",
+        "cwl-upgrader >= 1.2",
     ],
     setup_requires=[] + pytest_runner,
     tests_require=["pytest<7", "cwltool"],
