16a644591774fd1a2da62c73a2ec379ce3186b7f
Tomoya Tanjo <ttanjo@gmail.com>
2021-09-23 08:29:31 +0000

    Fix format and type annotations

--- cwl_utils/parser/__init__.py
+++ cwl_utils/parser/__init__.py
@@ -7,21 +7,26 @@ import cwl_utils.parser.parser_v1_1 as cwl_v1_1
 import cwl_utils.parser.parser_v1_2 as cwl_v1_2
 
 import os
-from typing import MutableMapping, MutableSequence, Union, Optional
+from typing import cast, Any, MutableMapping, MutableSequence, Union, Optional
 
-LoadingOptions = Union[cwl_v1_0.LoadingOptions, cwl_v1_1.LoadingOptions, cwl_v1_2.LoadingOptions]
+LoadingOptions = Union[
+    cwl_v1_0.LoadingOptions, cwl_v1_1.LoadingOptions, cwl_v1_2.LoadingOptions
+]
 Savable = Union[cwl_v1_0.Savable, cwl_v1_1.Savable, cwl_v1_2.Savable]
 save_type = Union[cwl_v1_0.save_type, cwl_v1_1.save_type, cwl_v1_2.save_type]
 
 
-def cwl_version(yaml: MutableMapping) -> Optional[str]:
-    if 'cwlVersion' not in list(yaml.keys()):
+def cwl_version(yaml: Any) -> Any:
+    if "cwlVersion" not in list(yaml.keys()):
         return None
-    return yaml['cwlVersion']
-        
+    return yaml["cwlVersion"]
 
-def load_document(doc: Union[str, MutableMapping], baseuri: Optional[str] = None,
-                  loadingOptions: Optional[LoadingOptions] = None) -> Union[Savable, MutableSequence[Savable]]:
+
+def load_document(
+    doc: Any,
+    baseuri: Optional[str] = None,
+    loadingOptions: Optional[LoadingOptions] = None,
+) -> Any:
     if baseuri is None:
         baseuri = cwl_v1_0.file_uri(os.getcwd()) + "/"
     if isinstance(doc, str):
@@ -29,39 +34,53 @@ def load_document(doc: Union[str, MutableMapping], baseuri: Optional[str] = None
     return load_document_by_yaml(doc, baseuri, loadingOptions)
 
 
-def load_document_by_string(string: str, uri: str, loadingOptions: Optional[LoadingOptions] = None) -> Union[Savable, MutableSequence[Savable]]:
+def load_document_by_string(
+    string: str, uri: str, loadingOptions: Optional[LoadingOptions] = None
+) -> Any:
     yaml = yaml_no_ts()
     result = yaml.load(string)
     return load_document_by_yaml(result, uri, loadingOptions)
 
 
-def load_document_by_yaml(yaml: MutableMapping, uri: str, loadingOptions: Optional[LoadingOptions] = None) -> Union[Savable, MutableSequence[Savable]]:
+def load_document_by_yaml(
+    yaml: Any, uri: str, loadingOptions: Optional[LoadingOptions] = None
+) -> Any:
     version = cwl_version(yaml)
-    if version == 'v1.0':
-        result = cwl_v1_0.load_document_by_yaml(yaml, uri, loadingOptions)
-    elif version == 'v1.1':
-        result = cwl_v1_1.load_document_by_yaml(yaml, uri, loadingOptions)
-    elif version == 'v1.2':
-        result = cwl_v1_2.load_document_by_yaml(yaml, uri, loadingOptions)
+    if version == "v1.0":
+        result = cwl_v1_0.load_document_by_yaml(
+            yaml, uri, cast(Optional[cwl_v1_0.LoadingOptions], loadingOptions)
+        )
+    elif version == "v1.1":
+        result = cwl_v1_1.load_document_by_yaml(
+            yaml, uri, cast(Optional[cwl_v1_1.LoadingOptions], loadingOptions)
+        )
+    elif version == "v1.2":
+        result = cwl_v1_2.load_document_by_yaml(
+            yaml, uri, cast(Optional[cwl_v1_2.LoadingOptions], loadingOptions)
+        )
     elif version is None:
         raise ValidationException("could not get the cwlVersion")
     else:
-        raise ValidationException("Version error. Did not recognise {} as a CWL version".format(version))
+        raise ValidationException(
+            "Version error. Did not recognise {} as a CWL version".format(version)
+        )
 
     if isinstance(result, MutableSequence):
         lst = []
         for r in result:
-            if 'cwlVersion' in r.attrs:
+            if "cwlVersion" in r.attrs:
                 r.cwlVersion = version
             lst.append(r)
         return lst
     return result
 
 
-def save(val: Optional[Union[Savable, MutableSequence[Savable]]],
-         top: bool = True,
-         base_url: str = "",
-         relative_uris: bool = True) -> save_type:
+def save(
+    val: Optional[Union[Savable, MutableSequence[Savable]]],
+    top: bool = True,
+    base_url: str = "",
+    relative_uris: bool = True,
+) -> Any:
     if (
         isinstance(val, cwl_v1_0.Savable)
         or isinstance(val, cwl_v1_1.Savable)
@@ -73,13 +92,15 @@ def save(val: Optional[Union[Savable, MutableSequence[Savable]]],
             save(v, top=top, base_url=base_url, relative_uris=relative_uris)
             for v in val
         ]
-        if top and all((isinstance(v, cwl_v1_0.Process)
-                        or isinstance(v, cwl_v1_1.Process)
-                        or isinstance(v, cwl_v1_2.Process) for v in val)):
-            return {
-                'cwlVersion': lst[0]['cwlVersion'],
-                '$graph': lst
-            }
+        if top and all(
+            (
+                isinstance(v, cwl_v1_0.Process)
+                or isinstance(v, cwl_v1_1.Process)
+                or isinstance(v, cwl_v1_2.Process)
+                for v in val
+            )
+        ):
+            return {"cwlVersion": lst[0]["cwlVersion"], "$graph": lst}
         return lst
     if isinstance(val, MutableMapping):
         newdict = {}
