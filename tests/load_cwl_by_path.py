9ab8afe16efa309c562ba5678f4ace87f28f2bb2
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-10-11 12:35:39 +0200

    put example under test

---
+++ tests/load_cwl_by_path.py
@@ -0,0 +1,20 @@
+"""This is the example from README.md, please synchronize all changes between the two."""
+# SPDX-License-Identifier: Apache-2.0
+from pathlib import Path
+from ruamel import yaml
+import sys
+
+from cwl_utils.parser import load_document_by_path, save
+
+# File Input - This is the only thing you will need to adjust or take in as an input to your function:
+cwl_file = Path("testdata/md5sum.cwl")  # or a plain string works as well
+
+# Import CWL Object
+cwl_obj = load_document_by_path(cwl_file)
+
+# View CWL Object
+print("List of object attributes:\n{}".format("\n".join(map(str, dir(cwl_obj)))))
+
+# Export CWL Object into a built-in typed object
+saved_obj = save(cwl_obj)
+print(f"Export of the loaded CWL object: {saved_obj}.")
---
+++ tests/test_examples.py
@@ -0,0 +1,18 @@
+"""Tests of example Python scripts."""
+import os
+from pathlib import Path
+
+
+def test_load_example() -> None:
+    """Test the load_cwl_by_path.py script."""
+    cwd = Path.cwd()
+    parent = Path(__file__).resolve().parent
+    os.chdir(parent.parent)
+    exec(
+        open(parent / "load_cwl_by_path.py").read(),
+        globals(),
+        locals(),
+    )
+    os.chdir(cwd)
+    result = locals()["saved_obj"]
+    assert result["class"] == "Workflow"
