864203fed5f7d5b4701fce83271d23e8ce506af8
Tomoya Tanjo <ttanjo@gmail.com>
2021-11-08 19:02:28 +0900

    Add cwl_utils.parser.latest (#119)

---
+++ cwl_utils/parser/latest.py
@@ -0,0 +1,1 @@
+from .cwl_v1_2 import *  # noqa: F403
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from ruamel import yaml
 
 from cwl_utils.parser import cwl_version, load_document, load_document_by_uri, save
+import cwl_utils.parser.latest as latest
 
 HERE = Path(__file__).resolve().parent
 TEST_v1_0_CWL = HERE / "../testdata/md5sum.cwl"
@@ -59,3 +60,12 @@ def test_save() -> None:
     saved_obj = save([cwl_obj10, cwl_obj12])
     ver = cwl_version(saved_obj)
     assert ver == "v1.2"
+
+
+def test_latest_parser() -> None:
+    """Test the `latest` parser is same as cwl_v1_2 (current latest) parser."""
+    uri = Path(TEST_v1_2_CWL).as_uri()
+    with open(TEST_v1_2_CWL, "r") as cwl_h:
+        yaml_obj12 = yaml.main.round_trip_load(cwl_h, preserve_quotes=True)
+    latest_cwl_obj = latest.load_document_by_yaml(yaml_obj12, uri)  # type: ignore
+    assert latest_cwl_obj.cwlVersion == "v1.2"
