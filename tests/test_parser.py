840b9decdc493b07fc4233f00fe03c9c2e3334ab
Tomoya Tanjo <ttanjo@gmail.com>
2021-11-08 07:50:53 +0000

    Add test for the latest parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 """Test the load and save functions for CWL."""
 from cwl_utils.parser import cwl_version, load_document, load_document_by_uri, save
+import cwl_utils.parser.latest as latest
 from pathlib import Path
 from ruamel import yaml
 
@@ -57,3 +58,12 @@ def test_save() -> None:
     saved_obj = save([cwl_obj10, cwl_obj12])
     ver = cwl_version(saved_obj)
     assert ver == "v1.2"
+
+
+def test_latest_parser() -> None:
+    """Test the `latest` parser is same as cwl_v1_2 (current latest) parser."""
+    uri = Path(TEST_v1_2_CWL).as_uri()
+    with open(TEST_v1_2_CWL, "r") as cwl_h:
+        yaml_obj12 = yaml.main.round_trip_load(cwl_h, preserve_quotes=True)
+    latest_cwl_obj = latest.load_document_by_yaml(yaml_obj12, uri)
+    assert latest_cwl_obj.cwlVersion == "v1.2"
