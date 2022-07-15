cc15530f6537bc48133ac5e35cbf09bdf9a9dd2a
Michael R. Crusoe <michael.crusoe@gmail.com>
2022-07-15 16:21:57 -0500

    reformat

--- cwl_utils/errors.py
+++ cwl_utils/errors.py
@@ -2,7 +2,6 @@ class ArrayMissingItems(BaseException):
     """From https://github.com/rabix/sbpack/blob/b8404a0859ffcbe1edae6d8f934e51847b003320/sbpack/lib.py"""
 
 
-
 class JavascriptException(Exception):
     pass
 
@@ -11,15 +10,12 @@ class MissingKeyField(BaseException):
     """From https://github.com/rabix/sbpack/blob/b8404a0859ffcbe1edae6d8f934e51847b003320/sbpack/lib.py"""
 
 
-
 class MissingTypeName(BaseException):
     """From https://github.com/rabix/sbpack/blob/b8404a0859ffcbe1edae6d8f934e51847b003320/sbpack/lib.py"""
 
 
-
 class RecordMissingFields(BaseException):
     """From https://github.com/rabix/sbpack/blob/b8404a0859ffcbe1edae6d8f934e51847b003320/sbpack/lib.py"""
-
 
 
 class SubstitutionError(Exception):
