d857e217011aa8d3c15092179b6ebfc0c3b767d7
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-01-27 11:57:56 +0100

    add offline graph-split test + fix

--- tests/test_graph_split.py
+++ tests/test_graph_split.py
@@ -8,12 +8,19 @@ import requests
 
 from cwl_utils.graph_split import graph_split
 
+from .util import get_data
+
 URI = "https://gist.githubusercontent.com/altairwei/6a0097db95cad23de36f825ed3b9f4b0/raw/83f332931c3093ee73554cd7f60054ce17d03239/rhapsody_wta_1.8.packed.cwl"
 
 
 def test_graph_split(tmp_path: Path) -> None:
     """Confirm that a user provided example produces no exception."""
-    os.chdir(tmp_path)
     sourceIO = StringIO(requests.get(URI).text)
     sourceIO.name = URI
-    graph_split(sourceIO, ".", "yaml", "main.cwl", True)
+    graph_split(sourceIO, tmp_path, "yaml", "main.cwl", True)
+
+
+def test_graph_split_offline(tmp_path: Path) -> None:
+    """Confirm that a local provided example produces no exception."""
+    with open(get_data("testdata/js-expr-req-wf.cwl")) as handle:
+        graph_split(handle, tmp_path, "yaml", "main.cwl", True)
