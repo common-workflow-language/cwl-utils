c7adc2ef11d4472f29a94f4a3129862dee0fd434
Michael R. Crusoe <michael.crusoe@gmail.com>
2024-09-08 19:23:21 +0200

    Regenerate the parsers

--- mypy-stubs/cachecontrol/caches/file_cache.pyi
+++ mypy-stubs/cachecontrol/caches/file_cache.pyi
@@ -1,31 +1,35 @@
-from os import PathLike
-from typing import ContextManager
+from datetime import datetime
+from pathlib import Path
+from typing import IO, ContextManager
 
-from ..cache import BaseCache as BaseCache
-from ..controller import CacheController as CacheController
+from cachecontrol.cache import SeparateBodyBaseCache
+from filelock import BaseFileLock
 
 class _LockClass:
     path: str
 
 _lock_class = ContextManager[_LockClass]
 
-class FileCache(BaseCache):
-    directory: str | PathLike[str]
+class _FileCacheMixin:
+    directory: str
     forever: bool
-    filemode: str
-    dirmode: str
-    lock_class: _lock_class
+    filemode: int
+    dirmode: int
+    lock_class: _lock_class | None = None
     def __init__(
         self,
-        directory: str | PathLike[str],
-        forever: bool = ...,
-        filemode: int = ...,
-        dirmode: int = ...,
-        use_dir_lock: bool | None = ...,
-        lock_class: _lock_class | None = ...,
+        directory: str | Path,
+        forever: bool = False,
+        filemode: int = 384,
+        dirmode: int = 448,
+        lock_class: type[BaseFileLock] | None = None,
     ) -> None: ...
     @staticmethod
     def encode(x: str) -> str: ...
-    def get(self, key: str) -> None | str: ...
-    def set(self, key: str, value: str) -> None: ...
+    def get(self, key: str) -> bytes | None: ...
+    def set(self, key: str, value: bytes, expires: int | datetime | None = None) -> None: ...
+
+class SeparateBodyFileCache(_FileCacheMixin, SeparateBodyBaseCache):
+    def get_body(self, key: str) -> IO[bytes] | None: ...
+    def set_body(self, key: str, body: bytes) -> None: ...
     def delete(self, key: str) -> None: ...
