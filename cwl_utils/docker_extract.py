81474d64a7a5a142a2e8710e57c50977cce41f34
Sameeul Samee <sameeul@gmail.com>
2024-12-07 06:58:19 -0500

    Do not add 'latest' tag if no tag is present

--- cwl_utils/docker_extract.py
+++ cwl_utils/docker_extract.py
@@ -96,8 +96,6 @@ def extract_docker_requirements(
 ) -> Iterator[cwl.DockerRequirement]:
     """Yield an iterator of the docker reqs, normalizing the pull request."""
     for req in extract_docker_reqs(process):
-        if isinstance(req.dockerPull, str) and ":" not in req.dockerPull:
-            req.dockerPull += ":latest"
         yield req
 
 
