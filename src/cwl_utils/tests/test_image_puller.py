5cb41b4491e6e43e7d228bd1aa1669cacacdb30c
Adam Novak <anovak@soe.ucsc.edu>
2026-07-23 18:09:51 -0400

    Add tests for backward-compatible name generation

--- src/cwl_utils/tests/test_image_puller.py
+++ src/cwl_utils/tests/test_image_puller.py
@@ -31,3 +31,47 @@ class TestSingularityImagePuller:
 
         assert get_name("some_name/repo:123") == f"some___name_s_repo:123{suffix}"
         assert get_name("some/name_repo:123") == f"some_s_name___repo:123{suffix}"
+
+    def test_get_image_alternate_names_match_old_cwltool(self) -> None:
+        """
+        Make sure alternate image names tries match those previously used by
+        cwltool 3.2.20260720092025.
+        """
+        if is_singularity_version_2_6():
+            suffix = ".img"
+        elif is_singularity_version_3_or_newer():
+            suffix = ".sif"
+        else:
+            raise Exception(
+                f"Don't know how to handle this version of singularity: {get_singularity_version()}."
+            )
+
+        def get_names(s: str) -> list[str]:
+            return SingularityImagePuller(
+                s, None, "", False
+            ).get_alternate_image_names()
+
+        assert f"debian:stable-slim{suffix}" in get_names("debian:stable-slim")
+        assert f"quay.io_user_image_latest{suffix}" in get_names("quay.io/user/image")
+
+    def test_get_image_alternate_names_match_old_cwl_utils(self) -> None:
+        """
+        Make sure alternate image names tries match those previously used by
+        cwl-utils 0.42
+        """
+        if is_singularity_version_2_6():
+            suffix = ".img"
+        elif is_singularity_version_3_or_newer():
+            suffix = ".sif"
+        else:
+            raise Exception(
+                f"Don't know how to handle this version of singularity: {get_singularity_version()}."
+            )
+
+        def get_names(s: str) -> list[str]:
+            return SingularityImagePuller(
+                s, None, "", False
+            ).get_alternate_image_names()
+
+        assert f"debian_stable-slim{suffix}" in get_names("debian:stable-slim")
+        assert f"quay.io_user_image{suffix}" in get_names("quay.io/user/image")
